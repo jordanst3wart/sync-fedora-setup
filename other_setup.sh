b368934dda3e204004a2047fbdc6b106114539ea
Jordan Stewart <jordan.stewart@versent.com.au>
2020-03-12 00:40:58 +1100

    Create other_setup.sh

---
+++ other_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Go
+# details: https://golang.org/doc/install
+# /etc/profile (for a system-wide installation) or $HOME/.profile: 
+export PATH=$PATH:/usr/local/go/bin
+tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+
+# local bin
+/home/jstewart/.local/bin
+
+# other data :|
